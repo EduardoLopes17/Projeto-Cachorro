1136998e8e714afdca50745e8ea5d8df2fa3e930
EduardoLopesssss <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-04-07 09:17:12 -0300

    add - Fiz todas as regras de Negocio

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+document.getElementById("formAdocao").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+  let CPF = document.getElementById("CPF").value;
+  let idade = document.getElementById("idade").value;
+  let cidade = document.getElementById("cidade").value;
+
+  let moradia = document.querySelector('input[name="moradia"]:checked');
+  let pet = document.querySelector('input[name="pet"]:checked');
+  let quintal = document.querySelector('input[name="quintal"]:checked');
+
+  let tempo = document.getElementById("tempo").value;
+  let termos = document.getElementById("termos");
+
+  if (nome.length < 3) return alert("Nome Inválido");
+
+  document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
